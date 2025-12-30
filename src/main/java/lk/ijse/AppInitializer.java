70c77975219e21bcbb4dce6556584d9ac5bb7569
dinushiaherath <dinushi.a.herath@gmail.com>
2025-12-30 17:05:57 +0530

    Removed unnecessary file reference

--- src/main/java/lk/ijse/AppInitializer.java
+++ src/main/java/lk/ijse/AppInitializer.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import lk.ijse.gear_rent_pro.util.DatabaseInitializer;
 
 public class AppInitializer extends Application {
 
